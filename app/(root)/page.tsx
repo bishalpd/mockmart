8b1d78e71239b90eee2b2e21f3dbb5fc9511078d
bishalpd <bishal.peter@apsissolutions.com>
2025-01-03 07:55:26 +0600

    first commit

---
+++ app/(root)/page.tsx
@@ -0,0 +1,8 @@
+import Image from "next/image";
+
+export const metadata = {
+  title: "Home",
+};
+export default function Home() {
+  return <>mock mart</>;
+}
---
+++ lib/constants/index.ts
@@ -0,0 +1,7 @@
+export const APP_NAME = process.env.NEXT_PUBLIC_APP_NAME || "MockMart";
+export const APP_DESCRIPTION =
+  process.env.NEXT_PUBLIC_APP_DESCRIPTION ||
+  "Your go-to demo e-commerce platform for showcasing online shopping experiences. Explore a seamless interface, product listings, and checkout flows for project demonstrations.";
+
+export const SERVER_URL =
+  process.env.NEXT_PUBLIC_SERVER_URL || "http://localhost:3000";
