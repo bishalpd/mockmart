d4e8c80458d2aedb7fc7d85803a7ad4e8527e4e8
bishal <bishalpd@gmail.com>
2025-05-10 17:55:36 +0600

    admin overview

--- lib/validators.ts
+++ lib/validators.ts
@@ -101,3 +101,9 @@ export const insertOrderItemSchema = z.object({
   price: currency,
   qty: z.number(),
 });
+
+// schema for updating the user profile
+export const updateProfileSchema = z.object({
+  name: z.string().min(3, "Name must be at least 3 character"),
+  email: z.string().min(3, "Email must be at least 3 characters"),
+});
